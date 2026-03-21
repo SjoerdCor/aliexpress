094b3702a9341ab306ff8b7369365d706d786fd7
Sjoerd Cornelissen <sjoerdcornelissen@gmail.com>
2026-03-21 12:48:06 +0100

    refactor main function

--- src/aliexpress/main.py
+++ src/aliexpress/main.py
@@ -45,6 +45,144 @@ def jsons_to_excel(folder, preferences, input_sheet, students_info):
             sa.to_excel()
 
 
+def _safe_read(fn, *, filetype, technical_message, catch=Exception):
+    try:
+        return fn()
+    except (errors.ValidationError, pa.errors.SchemaError):
+        raise
+    except catch as e:
+        raise errors.CouldNotReadFileError(
+            "could_not_read",
+            context={"filetype": filetype},
+            technical_message=technical_message,
+        ) from e
+
+
+def _read_groups(path):
+    return _safe_read(
+        lambda: datareader.read_groups_excel(path),
+        filetype="groepen",
+        technical_message="Could not read groups_to",
+    )
+
+
+def _read_preferences(path, groups_to):
+    def _inner():
+        processor = datareader.VoorkeurenProcessor(path)
+        preferences = processor.process(all_to_groups=list(groups_to.keys()))
+        return processor, preferences
+
+    return _safe_read(
+        _inner,
+        filetype="voorkeuren",
+        technical_message="Could not read preferences",
+    )
+
+
+def _read_not_together(path, students, groups_to):
+    return _safe_read(
+        lambda: datareader.read_not_together(path, students, len(groups_to)),
+        filetype="niet-samen",
+        technical_message="Could not read not_together",
+    )
+
+
+def _log_initial_state(groups_to, students_info, on_update):
+    df_groups = pd.DataFrame.from_dict(groups_to, orient="index")
+    logger.info(
+        "Current groups:\n%s",
+        df_groups.assign(Totaal=lambda df: df.sum("columns")),
+    )
+
+    df_students = pd.DataFrame.from_dict(students_info, orient="index")
+    sex_dist = df_students[["Jongen/meisje"]].value_counts()
+
+    on_update(
+        f"{len(df_students)} leerlingen te verdelen, "
+        f"waarvan {sex_dist.loc['Jongen'].squeeze()} jongens "
+        f"en {sex_dist.loc['Meisje'].squeeze()} meisjes"
+    )
+    logger.info("Current boy/girl distribution:\n%s", sex_dist)
+
+    on_update("Komen uit de volgende groepen:")
+    for group, value in df_students["Stamgroep"].value_counts().items():
+        on_update(f"{group}: {value}")
+
+
+def _check_feasibility(ps):
+    feas_prob = ps.calculate_feasibility()
+    if feas_prob.objective.value() <= 0:
+        return
+
+    slack_info = {
+        "SLACK_balanced_boys_girls_total": (
+            "Maximale verschil jongens/meisjes totale groep",
+            ps.max_imbalance_boys_girls_total,
+        ),
+        "SLACK_balanced_boys_girls_year": (
+            "Maximale verschil jongens/meisjes nieuwe jaarlaag",
+            ps.max_imbalance_boys_girls_year,
+        ),
+        "SLACK_diff_n_students_total": (
+            "Maximale verschil totale groepsgrootte",
+            ps.max_diff_n_students_total,
+        ),
+        "SLACK_diff_n_students_year": (
+            "Maximale verschil groepsgrootte nieuwe jaarlaag",
+            ps.max_diff_n_students_year,
+        ),
+        "SLACK_max_clique": (
+            "Maximale groep vanuit eerdere groep",
+            ps.max_clique,
+        ),
+        "SLACK_max_clique_sex": (
+            "Maximale groep jongens/meisjes vanuit eerdere groep",
+            ps.max_clique_sex,
+        ),
+    }
+
+    msg = []
+    variables = feas_prob.variablesDict()
+
+    for name, (label, base_value) in slack_info.items():
+        val = variables[name].value()
+        if val > 0:
+            msg.append(f"{label}: {round(base_value + val)} (+ {round(val)})")
+
+    raise errors.FeasibilityError(
+        "infeasible_problem",
+        context={"possible_improvement": "\n".join(msg)},
+        technical_message="Can not solve the problem for this class imbalance",
+    )
+
+
+def _solve_and_export(ps, preferences, processor, students_info):
+    logger.info("Finding first solution... lexmaxmin")
+    ps.run(save=False)
+    logger.info("Found solution")
+
+    with tempfile.NamedTemporaryFile(mode="w+", suffix=".json", delete=False) as tmp:
+        ps.prob.toJson(tmp.name)
+        tmp.flush()
+        sa = solutions.SolutionAnalyzer(
+            tmp.name, preferences, processor.input, students_info
+        )
+
+    output = BytesIO()
+    sa.to_excel(output)
+    output.seek(0)
+
+    dfs = {
+        "Groepsindeling": sa.display_groepsindeling(),
+        "Klassenoverzicht": sa.group_report,
+        "Overgangsmatrix": sa.display_transition_matrix(),
+        "Leerlingtevredenheid": sa.display_student_performance(),
+        "VervuldeWensen": sa.display_satisfied_preferences(),
+    }
+
+    return output, dfs
+
+
 def distribute_students_once(
     path_preferences=FILE_PREFERENCES,
     path_groups_to=FILE_GROUPS_TO,
@@ -60,62 +198,19 @@ def distribute_students_once(
             Takes a user friendly message and decides what to do with it for the calling
             function. By default, ignores them
     """
-    try:
-        groups_to = datareader.read_groups_excel(path_groups_to)
-    except pa.errors.SchemaError as e:
-        raise e
-    except Exception as e:
-        raise errors.CouldNotReadFileError(
-            "could_not_read",
-            context={"filetype": "groepen"},
-            technical_message="Could not read groups_to",
-        ) from e
-    try:
-        processor = datareader.VoorkeurenProcessor(path_preferences)
-        preferences = processor.process(all_to_groups=list(groups_to.keys()))
-    # TODO: remove ValidationError
-    except (errors.ValidationError, pa.errors.SchemaError) as e:
-        raise e
-    except Exception as e:
-        raise errors.CouldNotReadFileError(
-            "could_not_read",
-            context={"filetype": "voorkeuren"},
-            technical_message="Could not read preferences",
-        ) from e
+    groups_to = _read_groups(path_groups_to)
+    processor, preferences = _read_preferences(path_preferences, groups_to)
+
     students_info = processor.get_students_meta_info()
-    try:
-        not_together = datareader.read_not_together(
-            path_not_together, students_info.keys(), len(groups_to)
-        )
-    except pa.errors.SchemaError as e:
-        raise e
-    except Exception as e:
-        raise errors.CouldNotReadFileError(
-            "could_not_read",
-            context={"filetype": "niet-samen"},
-            technical_message="Could not read not_together",
-        ) from e
+    not_together = _read_not_together(
+        path_not_together, students_info.keys(), groups_to
+    )
     on_update("Alle bestanden zijn gevalideerd!")
     logger.info("All files read")
 
-    df_groups_to = pd.DataFrame.from_dict(groups_to, orient="index")
-    logger.info(
-        "Current groups:\n%s", df_groups_to.assign(Totaal=lambda df: df.sum("columns"))
-    )
+    _log_initial_state(groups_to, students_info, on_update)
 
-    df_students = pd.DataFrame.from_dict(students_info, orient="index")
-    sex_distribution = df_students[["Jongen/meisje"]].value_counts()
-    on_update(
-        f"{len(df_students)} leerlingen te verdelen, "
-        f"waarvan {sex_distribution.loc['Jongen'].squeeze()} jongens "
-        f"en {sex_distribution.loc['Meisje'].squeeze()} meisjes"
-    )
-    logger.info("Current boy/girl distribution:\n%s", sex_distribution)
-    on_update("Komen uit de volgende groepen:")
-    for group, value in df_students["Stamgroep"].value_counts().items():
-        on_update(f"{group}: {value}")
-
-    ps_lexmaxmin = problemsolver.ProblemSolver(
+    ps = problemsolver.ProblemSolver(
         preferences,
         students_info,
         groups_to,
@@ -123,78 +218,14 @@ def distribute_students_once(
         optimize="lexmaxmin",
         **kwargs,
     )
-    feas_prob = ps_lexmaxmin.calculate_feasibility()
-    if feas_prob.objective.value() > 0:
-        slack_var_dct = {
-            "SLACK_balanced_boys_girls_total": {
-                "name": "Maximale verschil jongens/meisjes totale groep",
-                "attr_value": ps_lexmaxmin.max_imbalance_boys_girls_total,
-            },
-            "SLACK_balanced_boys_girls_year": {
-                "name": "Maximale verschil jongens/meisjes nieuwe jaarlaag",
-                "attr_value": ps_lexmaxmin.max_imbalance_boys_girls_year,
-            },
-            "SLACK_diff_n_students_total": {
-                "name": "Maximale verschil totale groepsgrootte",
-                "attr_value": ps_lexmaxmin.max_diff_n_students_total,
-            },
-            "SLACK_diff_n_students_year": {
-                "name": "Maximale verschil groepsgrootte nieuwe jaarlaag",
-                "attr_value": ps_lexmaxmin.max_diff_n_students_year,
-            },
-            "SLACK_max_clique": {
-                "name": "Maximale groep vanuit eerdere groep",
-                "attr_value": ps_lexmaxmin.max_clique,
-            },
-            "SLACK_max_clique_sex": {
-                "name": "Maximale groep jongens/meisjes vanuit eerdere groep",
-                "attr_value": ps_lexmaxmin.max_clique_sex,
-            },
-        }
-
-        msg = ""
-        for slack_var_name, dct in slack_var_dct.items():
-            v = feas_prob.variablesDict()[slack_var_name]
-            if v.value() > 0:
-                msg += (
-                    f'{dct["name"]}: {round(dct["attr_value"] + v.value())}'
-                    f" (+ {round(v.value())})\n"
-                )
-
-        raise errors.FeasibilityError(
-            "infeasible_problem",
-            context={"possible_improvement": msg},
-            technical_message="Can not solve the problem for this class imbalance",
-        )
+    _check_feasibility(ps)
     on_update("Bepaald dat probleem oplosbaar is!")
+
     on_update("Aan de slag! Groepen indelen...")
     logger.info("Finding first solution... lexmaxmin")
-    ps_lexmaxmin.run(save=False)
-    on_update("Groepsindeling gemaakt!")
-    on_update("Groepsindeling wegschrijven...")
-    logger.info("Found solution")
-
-    with tempfile.NamedTemporaryFile(mode="w+", suffix=".json", delete=False) as tmp:
-        ps_lexmaxmin.prob.toJson(tmp.name)
-        tmp.flush()
-        sa = solutions.SolutionAnalyzer(
-            tmp.name, preferences, processor.input, students_info
-        )
-    logger.info("Lexmaxmin done!")
-
-    output = BytesIO()
-    sa.to_excel(output)
-    output.seek(0)
+    output, dfs = _solve_and_export(ps, preferences, processor, students_info)
     logger.info("Done!")
     on_update("Klaar!")
-
-    dfs = {
-        "Groepsindeling": sa.display_groepsindeling(),
-        "Klassenoverzicht": sa.group_report,
-        "Overgangsmatrix": sa.display_transition_matrix(),
-        "Leerlingtevredenheid": sa.display_student_performance(),
-        "VervuldeWensen": sa.display_satisfied_preferences(),
-    }
     return {"download": output, "dataframes": dfs}
 
 
