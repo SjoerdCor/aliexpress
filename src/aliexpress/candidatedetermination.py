0a1cd8657b4b60f277bf6836ea866d69f2d2c61f
Sjoerd Cornelissen <sjoerdcornelissen@gmail.com>
2026-03-25 14:05:25 +0100

    Remove unused function

--- src/aliexpress/candidatedetermination.py
+++ src/aliexpress/candidatedetermination.py
@@ -1,11 +1,9 @@
 """Functions to select students and groups to be selected for distribution"""
 
-from collections import Counter
-
 import pandas as pd
 
 from aliexpress.datareader import clean_name
-from aliexpress.errors import DuplicateGroupError, DuplicateNameError
+from aliexpress.errors import DuplicateNameError
 
 
 def get_candidates(df: pd.DataFrame, jaargroep: int) -> list:
@@ -62,35 +60,6 @@ def handle_edexml_upload(df: pd.DataFrame, jaargroep: int):
     groups_from = get_groups_from(df, jaargroep)
     groups_to = get_groups_to(df, jaargroep)
     return candidates, groups_from, groups_to
-
-
-def _build_groups_summary(existing_groups, new_groups):
-    """Build a summary of groups with counts of boys and girls"""
-    existing_groups = {
-        clean_name(k, full_clean=False): v for k, v in existing_groups.items()
-    }
-    new_groups = [clean_name(g, full_clean=False) for g in new_groups]
-
-    counts = Counter(new_groups)
-    dupes_in_new_groups = [item for item, count in counts.items() if count > 1]
-    overlap_new_groups_old_groups = set(existing_groups.keys()) & set(new_groups)
-    if dupes_in_new_groups or overlap_new_groups_old_groups:
-        duplicate_groupnames = dupes_in_new_groups + list(overlap_new_groups_old_groups)
-        raise DuplicateGroupError(
-            "duplicate_group",
-            {"duplicate_groups": duplicate_groupnames},
-            f"Found duplicate groups in {existing_groups.keys()} and {new_groups}",
-        )
-
-    groups_to = {}
-    for g, lst in existing_groups.items():
-        c = Counter(lst)
-        groups_to[g] = {"Jongens": c.get("Jongen", 0), "Meisjes": c.get("Meisje", 0)}
-
-    for g in new_groups:
-        groups_to.setdefault(g, {"Jongens": 0, "Meisjes": 0})
-
-    return groups_to
 
 
 def combine_students(
